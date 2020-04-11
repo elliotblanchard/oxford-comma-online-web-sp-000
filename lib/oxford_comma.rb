2a77436fe1caad12ab624e80bda068e654ef83dc
Elliot Blanchard <public@elliotblanchard.com>
2020-04-11 17:10:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-  result_string = array.join(", ")
+  array_minus_last = array.pop
+  result_string = array_minus_last.join(", ")
+  result_string = result_string << ", and "
+  result_string = result_string << array.last
 end
